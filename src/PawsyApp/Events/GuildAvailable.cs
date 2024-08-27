3443582f769512b73ab9b88032baa649044c97dd
Robyn <rnixon98@gmail.com>
2024-08-27 09:03:43 -0500

    fix: unneeded usings

--- src/PawsyApp/Events/GuildAvailable.cs
+++ src/PawsyApp/Events/GuildAvailable.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using PawsyApp.Utils;
 using PawsyApp.GuildStorage;
 using System.Text.Json;
-using System.Reflection;
 using PawsyApp.Settings;
 
 namespace PawsyApp.Events;
