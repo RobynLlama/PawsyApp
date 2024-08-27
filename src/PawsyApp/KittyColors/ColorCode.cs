8294a8af266073509aebbad18306bf7cbf29ec7d
Robyn <rnixon98@gmail.com>
2024-08-27 17:44:37 -0500

    feat: pawsy can use cute colors now

---
+++ src/PawsyApp/KittyColors/ColorCode.cs
@@ -0,0 +1,13 @@
+namespace PawsyApp.KittyColors;
+
+public enum ColorCode
+{
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+}
---
+++ src/PawsyApp/KittyColors/KittyColor.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace PawsyApp.KittyColors;
+
+internal static class KittyColor
+{
+    internal static string reset = "\x1B[0m";
+    internal static string GetPrintableColor(ColorCode code, bool background = false)
+    {
+        int ansii;
+
+        if (background)
+            ansii = 40;
+        else
+            ansii = 30;
+
+        return $"\x1B[{ansii + (int)code}m";
+    }
+
+    internal static string WrapInColor(string? input, ColorCode foreground)
+    {
+        if (input is null)
+            return string.Empty;
+
+        return $"{GetPrintableColor(foreground)}{input}{reset}";
+    }
+}
