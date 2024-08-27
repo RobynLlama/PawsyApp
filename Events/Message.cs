31c6be8649d4f6feb0cb3c6acb5057d179f7c980
Robyn <rnixon98@gmail.com>
2024-08-27 05:05:28 -0500

    style: un-nest a bunch of nested code and use pattern matching for better cast safety

--- Events/Message.cs
+++ Events/Message.cs
@@ -18,41 +18,51 @@ internal class MessageEvent
             WriteLog.Cutely("Pawsy heard a DM!", [
             ("Author", message.Author.GlobalName),
             ("CleanContent", message.CleanContent),
+            ]);
+
+            await DMchannel.SendMessageAsync("Hi! I'm Pawsy, the cutie kitty app here to keep you safe and sound! Sorry, but I'm really shy in DMs ≧◡≦ please just talk to me on the server, okay?");
+            return;
+        }
+
+        if (message.Channel is not SocketGuildChannel guildChannel)
+            return;
+
+        var guild = guildChannel.Guild;
+
+        WriteLog.Cutely("Pawsy heard this!", [
+        ("Author", message.Author.GlobalName),
+            ("CleanContent", message.CleanContent),
+            ("Channel", guildChannel.Name),
             ("Guild", guild.Name ?? "Unknown"),
             ]);
 
-            if (PawsyProgram.SettingsStorage.TryGetValue(guild.Id, out var settings))
+        if (!PawsyProgram.SettingsStorage.TryGetValue(guild.Id, out var settings))
+            return;
+
+        if (guild.GetChannel(settings.LoggingChannelID) is not SocketTextChannel channel)
+            return;
+
+        foreach (var item in settings.rules)
+        {
+            if (item.Match(message.CleanContent))
             {
+                if (item.warn_staff)
+                    Chirp(channel, message, item);
+                //await message.Channel.SendMessageAsync(text: "Oopsie daisy! (✿◠‿◠) Your message got deleted for using naughty words. Pwease keep it pawsitive and kind! Let's keep our chat fun and fwiendly~ ≧◡≦");
+                if (item.send_response)
+                    await message.Channel.SendMessageAsync(text: item.response);
 
-                SocketTextChannel channel = (SocketTextChannel)guild.GetChannel(settings.LoggingChannelID);
+                if (item.delete_message)
+                    await message.DeleteAsync();
 
-                if (channel is not null)
-                {
-                    foreach (var item in settings.rules)
-                    {
-                        if (item.Match(message.CleanContent))
-                        {
-                            if (item.warn_staff)
-                                Chirp(channel, message, item);
-                            //await message.Channel.SendMessageAsync(text: "Oopsie daisy! (✿◠‿◠) Your message got deleted for using naughty words. Pwease keep it pawsitive and kind! Let's keep our chat fun and fwiendly~ ≧◡≦");
-                            if (item.send_response)
-                                await message.Channel.SendMessageAsync(text: item.response);
-
-                            if (item.delete_message)
-                                await message.DeleteAsync();
-
-                            break;
-                        }
-                    }
-
-
-                    static void Chirp(SocketTextChannel channel, SocketMessage message, RuleBundle violation)
-                    {
-                        Embed embed = new EmbedBuilder().WithAuthor("Pawsy").WithTitle("Detected message").WithDescription($"{message.Author}({message.Author.Id})\n\nLink: <@{message.Author.Id}>\nContents: {message.CleanContent}").WithColor(violation.color_R, violation.color_G, violation.color_B).WithFooter($"Rule: {violation.name}").Build();
-                        channel.SendMessageAsync(embed: embed);
-                    }
-                }
+                break;
             }
+        }
+
+        static void Chirp(SocketTextChannel channel, SocketMessage message, RuleBundle violation)
+        {
+            Embed embed = new EmbedBuilder().WithAuthor("Pawsy").WithTitle("Detected message").WithDescription($"{message.Author}({message.Author.Id})\n\nLink: <@{message.Author.Id}>\nContents: {message.CleanContent}").WithColor(violation.color_R, violation.color_G, violation.color_B).WithFooter($"Rule: {violation.name}").Build();
+            channel.SendMessageAsync(embed: embed);
         }
 
         return;
