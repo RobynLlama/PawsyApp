1e991947269c8446d9cd840a6197d08b3b7e7512
Robyn <rnixon98@gmail.com>
2024-08-29 06:25:57 -0500

    fix: filter the pawsy message announcer

--- src/PawsyApp/Events/Message.cs
+++ src/PawsyApp/Events/Message.cs
@@ -13,6 +13,11 @@ internal class MessageEvent
 {
     internal static Task Respond(SocketMessage message)
     {
+
+        //Filter out bots, system and webhook message
+        if (message.Author.IsBot || message.Author.IsWebhook || message.Source == MessageSource.System)
+            return Task.CompletedTask;
+
         WriteLog.Cutely("Pawsy heard this!", [
             ("Author", (message.Author as SocketGuildUser)?.Nickname ?? message.Author.GlobalName ?? message.Author.Username ?? "Nobody"),
             ("CleanContent", message.CleanContent),
