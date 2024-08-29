bcb9583068e8d5d29d72cffbebe0b8b1a688c33f
Robyn <rnixon98@gmail.com>
2024-08-28 22:28:24 -0500

    fix: implement module name

--- src/PawsyApp/PawsyCore/Pawsy.cs
+++ src/PawsyApp/PawsyCore/Pawsy.cs
@@ -11,6 +11,9 @@ public class Pawsy : IModuleIdent
     IModule? IModule.Owner { get => _owner; set => _owner = value; }
     public List<IModule> Modules => myModules;
     public ulong ID { get => 0; set { return; } }
+
+    public string Name => "PawsyCore";
+
     private readonly List<IModule> myModules = [];
     void IModule.Activate()
     {
