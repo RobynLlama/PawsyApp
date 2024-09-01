4e30f2591f02aa620e98a8906615de6d3a1a2559
Robyn <rnixon98@gmail.com>
2024-09-01 04:12:24 -0500

    feat: add config to modder role checker module

--- src/PawsyApp/PawsyCore/Modules/GuildModules/ModderRoleCheckerModule.cs
+++ src/PawsyApp/PawsyCore/Modules/GuildModules/ModderRoleCheckerModule.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -10,7 +12,7 @@ internal class ModderRoleCheckerModule : GuildModule
 {
     protected ModderRoleCheckerSettings Settings;
 
-    public ModderRoleCheckerModule(Guild Owner) : base(Owner, "modder-role")
+    public ModderRoleCheckerModule(Guild Owner) : base(Owner, "modder-role", declaresConfig: true)
     {
         Settings = (this as ISettingsOwner).LoadSettings<ModderRoleCheckerSettings>();
     }
@@ -23,6 +25,87 @@ internal class ModderRoleCheckerModule : GuildModule
     public override void OnModuleDeactivation()
     {
         Owner.OnGuildThreadCreated -= ThreadCreated;
+    }
+
+    public override void OnModuleDeclareConfig(SlashCommandOptionBuilder rootConfig)
+    {
+        rootConfig
+        .AddOption(
+            new SlashCommandOptionBuilder()
+            .WithType(ApplicationCommandOptionType.Channel)
+            .WithName("alert-channel")
+            .WithDescription("The channel where Pawsy will alert staff")
+        )
+        .AddOption(
+            new SlashCommandOptionBuilder()
+            .WithType(ApplicationCommandOptionType.Role)
+            .WithName("modding-role")
+            .WithDescription("The role Pawsy will look for")
+        )
+        .AddOption(
+            new SlashCommandOptionBuilder()
+            .WithType(ApplicationCommandOptionType.Channel)
+            .WithName("watch-channel")
+            .WithDescription("The channel Pawsy should watch for threads in")
+        );
+    }
+
+    public override Task OnConfigUpdated(SocketSlashCommand command, SocketSlashCommandDataOption options)
+    {
+
+        List<string> output = [];
+
+        foreach (var item in options.Options)
+        {
+            output.Add($"{item.Name} : {SetOption(item.Name, item.Value)}");
+        }
+
+        string SetOption(string optionName, object optionValue)
+        {
+            switch (optionName)
+            {
+                case "alert-channel":
+                    if (optionValue is not SocketTextChannel alertChannel)
+                    {
+                        return "Only text channels, please and thank mew";
+                    }
+
+                    Settings.AlertChannel = alertChannel.Id;
+                    (Settings as ISettings).Save<ModderRoleCheckerSettings>(this);
+                    return $"Set alert channel to <#{alertChannel.Id}>";
+                case "watch-channel":
+                    if (optionValue is not SocketGuildChannel watchChannel)
+                    {
+                        return "Only text channels, please and thank mew";
+                    }
+
+                    Settings.ModdingChannel = watchChannel.Id;
+                    (Settings as ISettings).Save<ModderRoleCheckerSettings>(this);
+                    return $"Set watch channel to <#{watchChannel.Id}>";
+                case "modding-role":
+                    if (optionValue is not SocketRole modderRole)
+                    {
+                        return "Send only role IDs, please and thank mew";
+                    }
+
+                    Settings.ModderRoleID = modderRole.Id;
+                    (Settings as ISettings).Save<ModderRoleCheckerSettings>(this);
+                    return $"Set modder role to <@&{modderRole.Id}>";
+                default:
+                    return "Something went wrong in HandleConfig";
+            }
+        }
+
+        StringBuilder sb = new("## Settings Changed:");
+
+        foreach (var item in output)
+        {
+            sb.Append('\n');
+            sb.Append("- ");
+            sb.Append(item);
+        }
+
+        return command.RespondAsync(sb.ToString());
     }
 
     private async Task ThreadCreated(SocketThreadChannel channel)
