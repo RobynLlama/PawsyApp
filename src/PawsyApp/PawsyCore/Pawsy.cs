9756aae1004168e7c3e602fd6657858945e555ee
Robyn <rnixon98@gmail.com>
2024-09-02 00:21:31 -0500

    refactor: Move socket management to the pawsy core object

--- src/PawsyApp/PawsyCore/Pawsy.cs
+++ src/PawsyApp/PawsyCore/Pawsy.cs
@@ -2,23 +2,155 @@ using System.Collections.Concurrent;
 using System;
 using System.IO;
 using System.Collections.Generic;
-using PawsyApp.Utils;
+using System.Text;
 using Discord.WebSocket;
+using Discord.Rest;
+using Discord;
+using System.Threading.Tasks;
+using PawsyApp.KittyColors;
 
 namespace PawsyApp.PawsyCore;
 
-internal class Pawsy : IUniqueCollection<ulong>
+internal class Pawsy : IUniqueCollection<ulong>, IUnique<ulong>
 {
-    //Eventually migrate socket connection here
-    public static string BaseConfigDir { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Pawsy");
-    protected ConcurrentBag<Guild> Guilds = [];
+    public readonly DiscordSocketClient SocketClient = new(new DiscordSocketConfig
+    {
+        MessageCacheSize = 100,
+        LogLevel = LogSeverity.Info,
+        AlwaysDownloadUsers = true,
+        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent | GatewayIntents.GuildMembers & ~GatewayIntents.GuildInvites & ~GatewayIntents.GuildScheduledEvents,
+    });
+    public readonly DiscordRestClient RestClient = new(new DiscordRestConfig
+    {
+        LogLevel = LogSeverity.Info
+    });
     public IEnumerable<IUnique<ulong>> UniqueCollection => Guilds;
+    public ulong ID { get => PawsyID; }
+    public string Name { get; } = "Pawsy";
+    public static string BaseConfigDir { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Pawsy");
+
+    protected ConcurrentBag<Guild> Guilds = [];
+    private readonly String? token = Environment.GetEnvironmentVariable("PAWSY_AUTH");
+    private readonly ulong PawsyID;
+    private static ulong NextPawsyID = 0;
 
     public Pawsy()
     {
+
+        PawsyID = GetNextID();
+
         DirectoryInfo config = new(BaseConfigDir);
         if (!config.Exists)
             config.Create();
+
+        SocketClient.GuildAvailable += GuildAvailable;
+        SocketClient.MessageReceived += MessageReceived;
+        SocketClient.Log += SocketLog;
+
+        if (token is not null)
+        {
+            LogAppendLine(Name, "Logging in with token");
+            SocketClient.LoginAsync(TokenType.Bot, token);
+            //RestClient.LoginAsync(TokenType.Bot, token);
+            SocketClient.StartAsync();
+        }
+        else
+        {
+            LogAppendLine(Name, "Token not found");
+        }
+    }
+
+    public Task LogAppendContext(string source, object message, (object ContextName, object ContextValue)[] context) => WriteLogInternal.AppendContext($"Pawsy{ID}->{source}", message, context);
+    public Task LogAppend(string source, object message) => WriteLogInternal.Append($"Pawsy~{ID}->{source}", message);
+    public Task LogAppendLine(string source, object message) => LogAppend(source, message + "\n");
+
+    private static class WriteLogInternal
+    {
+        public static Task AppendContext(string source, object msg, (object ContextName, object ContextValue)[] context)
+        {
+            StringBuilder sb = new(msg.ToString());
+            sb.AppendLine();
+            var dump = false;
+
+            foreach (var (ContextName, ContextValue) in context)
+            {
+                if (ContextName is null)
+                {
+                    sb.AppendLine(KittyColor.WrapInColor("ContextName is null", ColorCode.Red));
+                    dump = true;
+                    continue;
+                }
+                if (ContextValue is null)
+                {
+                    sb.AppendLine(KittyColor.WrapInColor("ContextValue is null", ColorCode.Red));
+                    dump = true;
+                    continue;
+                }
+
+
+                sb.Append("  ");
+                sb.Append(KittyColor.WrapInColor(ContextName.ToString(), ColorCode.Cyan));
+                sb.Append(": ");
+                sb.AppendLine(ContextValue.ToString());
+            }
+
+            if (dump)
+            {
+                using StreamWriter writer = new("pawsy-errors.log", true);
+                writer.WriteLine(sb);
+            }
+
+            sb.AppendLine();
+            return Append(source, sb);
+        }
+
+        public static Task Append(string source, object msg)
+        {
+            Console.Write($"[{KittyColor.WrapInColor(source, ColorCode.Magenta)}]  {msg}");
+            return Task.CompletedTask;
+        }
+    }
+
+    private static ulong GetNextID() => ++NextPawsyID;
+    private Task SocketLog(LogMessage message)
+    {
+        Console.WriteLine($"[{KittyColor.WrapInColor("Socket", ColorCode.Yellow)}]  {message}");
+        return Task.CompletedTask;
+    }
+
+    private async Task MessageReceived(SocketMessage message)
+    {
+        //Filter out bots, system and webhook message
+        if (message.Author.IsBot || message.Author.IsWebhook || message.Source == MessageSource.System)
+            return;
+
+        await LogAppendContext(Name, "Heard Something", [
+            ("Author", (message.Author as SocketGuildUser)?.Nickname ?? message.Author.GlobalName ?? message.Author.Username ?? "Nobody"),
+            ("CleanContent", message.CleanContent),
+            ("Channel", message.Channel.Name),
+            ("Guild", (message.Channel as SocketGuildChannel)?.Guild.Name ?? "No Guild"),
+            ]);
+
+        return;
+    }
+
+    private async Task GuildAvailable(SocketGuild guild)
+    {
+        var tasks = new List<Task>
+        {
+            LogAppendContext(Name, "Guild Available", [
+            ("GuildID", guild.Id),
+            ("GuildName", guild.Name)
+            ]),
+
+            //clear local commands to force the list to refresh
+            //guild.DeleteApplicationCommandsAsync()
+        };
+
+        AddOrGetGuild(guild);
+
+        await Task.WhenAll(tasks);
+        return;
     }
 
     public Guild AddOrGetGuild(SocketGuild guild)
@@ -28,15 +160,15 @@ internal class Pawsy : IUniqueCollection<ulong>
 
         if (item is null)
         {
-            WriteLog.LineNormal($"Creating Guild Instance for {ID}");
-            Guild newItem = new(guild);
+            LogAppendLine(Name, $"Creating Guild Instance for {ID}");
+            Guild newItem = new(guild, this);
             Guilds.Add(newItem);
             return newItem;
         }
 
         if (item is Guild gItem)
         {
-            WriteLog.LineNormal($"Returning Guild Instance for {ID}");
+            LogAppendLine(Name, $"Returning Guild Instance for {ID}");
             return gItem;
         }
 
--- src/PawsyApp/Program.cs
+++ src/PawsyApp/Program.cs
@@ -1,60 +1,12 @@
-﻿using OpenAI_API;
-using System;
-using System.Threading.Tasks;
-using Discord;
-using Discord.WebSocket;
-using PawsyApp.Events;
-using Discord.Rest;
+﻿using System.Threading.Tasks;
 using PawsyApp.PawsyCore;
 
 namespace PawsyApp;
 public class PawsyProgram
 {
-    internal static DiscordSocketClient SocketClient = new(new DiscordSocketConfig
-    {
-        MessageCacheSize = 100,
-        LogLevel = LogSeverity.Info,
-        AlwaysDownloadUsers = true,
-        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent | GatewayIntents.GuildMembers & ~GatewayIntents.GuildInvites & ~GatewayIntents.GuildScheduledEvents,
-    });
-
-    internal static DiscordRestClient RestClient = new(new DiscordRestConfig
-    {
-        LogLevel = LogSeverity.Info
-    });
-
-    internal static Pawsy Pawsy = new();
-    internal static OpenAIAPI api = new(Environment.GetEnvironmentVariable("OPENAI_AUTH"));
-
     public static async Task Main()
     {
-        SocketClient.Log += LogEvent.SocketRespond;
-        SocketClient.MessageReceived += MessageEvent.Respond;
-        SocketClient.Ready += ClientReady.Respond;
-        SocketClient.GuildAvailable += GuildAvailable.Respond;
-        RestClient.Log += LogEvent.RestRespond;
-        SocketClient.ThreadCreated += ThreadEvent.Respond;
-
-        //Get token from env
-        String? token = Environment.GetEnvironmentVariable("PAWSY_AUTH");
-
-        if (token is not null)
-        {
-            Console.WriteLine("Logging in with token");
-            await SocketClient.LoginAsync(TokenType.Bot, token);
-            await RestClient.LoginAsync(TokenType.Bot, token);
-            await SocketClient.StartAsync();
-
-            while (true)
-            {
-                await Task.Delay(250);
-            }
-        }
-        else
-        {
-            Console.WriteLine("Token not found");
-            return;
-        }
-
+        Pawsy pawsy = new();
+        await Task.Delay(-1);
     }
 }
