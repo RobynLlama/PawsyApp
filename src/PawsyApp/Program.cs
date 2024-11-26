d7cf5014366933ea6efaca53d39685f0c1142ed1
Robyn <rnixon98@gmail.com>
2024-11-26 05:46:00 -0600

    feat: add informational version display to pawsy startup

--- src/PawsyApp/Program.cs
+++ src/PawsyApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
 using PawsyApp.PawsyCore;
 
 namespace PawsyApp;
@@ -6,6 +8,14 @@ public class PawsyProgram
 {
     public static async Task Main()
     {
+
+        // Get the assembly informational version
+        var informationalVersion = Assembly.GetExecutingAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            ?.InformationalVersion ?? "Unknown version";
+
+        Console.WriteLine($"Pawsy version {informationalVersion}");
+
         Pawsy pawsy = new();
         await Task.Delay(-1);
     }
