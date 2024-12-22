1e075ed062224b157a1a2381e44bef85e2c269e0
Robyn <rnixon98@gmail.com>
2024-12-22 17:05:29 -0600

    feat: add command handlers

--- src/PawsyModules/PinBot/PinBot.cs
+++ src/PawsyModules/PinBot/PinBot.cs
@@ -1,3 +1,5 @@
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -84,19 +86,43 @@ public class PinBot : GuildModule
 
         switch (optionName)
         {
-            case "meow-limit":
-                if (optionValue is not int optionMax)
+            case "pin-role":
+                if (optionValue is not ulong roleID)
+                    return command.RespondAsync("Somehow that role is invalid");
+
+                if (Settings.RolesWithPerms.ContainsKey(roleID))
                 {
-                    return command.RespondAsync("I don't think that's a number, meow!", ephemeral: true);
+                    Settings.RolesWithPerms.Remove(roleID, out var _);
+                    command.RespondAsync($"Removed permissions from <@&{roleID}>");
+                }
+                else
+                {
+                    Settings.RolesWithPerms.TryAdd(roleID, true);
+                    command.RespondAsync($"Added permissions to <@&{roleID}>");
                 }
 
-                Settings.MeowLimit = (int)optionMax;
                 (Settings as ISettings).Save<PinBotSettings>(this);
 
-                return command.RespondAsync($"Set meow limit to {optionMax}");
+                return Task.CompletedTask;
             default:
                 return command.RespondAsync("Something went wrong in HandleConfig", ephemeral: true); ;
         }
+    }
+
+    protected (ulong ChannelID, ulong MessageID) ParseMessageLink(string link)
+    {
+        return (0u, 0u);
+    }
+
+    protected bool HasPermissions(SocketGuildUser user)
+    {
+        foreach (var role in user.Roles)
+        {
+            if (Settings.RolesWithPerms.ContainsKey(role.Id))
+                return true;
+        }
+
+        return false;
     }
 
     private Task ModuleCommandHandler(SocketSlashCommand command)
@@ -105,13 +131,75 @@ public class PinBot : GuildModule
         if (Settings is null)
             return command.RespondAsync("Settings are null in command handler", ephemeral: true);
 
+        if (!Owner.TryGetTarget(out var owner))
+            return command.RespondAsync("Unable to locate module owner in command handler", ephemeral: true);
+
         var options = command.Data.Options.First();
+        var link = options.Options.First().Value.ToString();
+
+        if (link is null)
+            return command.RespondAsync("Invalid link in command handler");
+
+        var (ChannelID, MessageID) = ParseMessageLink(link);
+
+        if (ChannelID == 0u || MessageID == 0)
+            return command.RespondAsync("Unable to resolve that message, sorry", ephemeral: true);
+
+        if (owner.DiscordGuild.GetChannel(ChannelID) is not SocketTextChannel tChannel)
+            return command.RespondAsync("Unable to resolve a text channel from that link", ephemeral: true);
+
+        if (tChannel.GetMessageAsync(MessageID).Result is not IUserMessage sMessage || sMessage is null)
+            return command.RespondAsync("I'm Unable to find or see that message", ephemeral: true);
+
         var commandName = options.Name;
+
+        //check user perms
+        if (command.User is not SocketGuildUser gUser)
+            return command.RespondAsync("Run this command from within a guild, please", ephemeral: true);
+
+        LogAppendContext("PinBot accessed", [
+            ("User", gUser.Id),
+            ("Name", gUser.DisplayName),
+            ("Link", link),
+            ("ChannelID", ChannelID),
+            ("MessageID", MessageID)
+        ]);
+
+        if (!HasPermissions(gUser))
+            return command.RespondAsync("You don't have permission to use this command, meow", ephemeral: true);
 
         switch (commandName)
         {
-            case "meow":
-                return command.RespondAsync($"Meow!");
+            case "pin":
+                if (sMessage.IsPinned)
+                    return command.RespondAsync("This message is already pinned", ephemeral: true);
+
+                try
+                {
+                    sMessage.PinAsync();
+                }
+                catch (Exception ex)
+                {
+                    LogAppendLine($"Encountered an error while pinning {ex}");
+                    return command.RespondAsync($"Exception encountered while pinning message {ex.GetType().Name}", ephemeral: true);
+                }
+
+                return command.RespondAsync("Done, message pinned", ephemeral: true);
+            case "unpin":
+                if (!sMessage.IsPinned)
+                    return command.RespondAsync("This message is not pinned", ephemeral: true);
+
+                try
+                {
+                    sMessage.UnpinAsync();
+                }
+                catch (Exception ex)
+                {
+                    LogAppendLine($"Encountered an error while pinning {ex}");
+                    return command.RespondAsync($"Exception encountered while pinning message {ex.GetType().Name}", ephemeral: true);
+                }
+
+                return command.RespondAsync("Done, message pinned", ephemeral: true);
             default:
                 return command.RespondAsync("Something went wrong in command handler", ephemeral: true); ;
         }
