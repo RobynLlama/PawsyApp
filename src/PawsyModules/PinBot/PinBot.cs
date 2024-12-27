52790b7c30c5a2b1470aadb16b65e323fbab908c
MasterAli2 <seperoa2li@gmail.com>
2024-12-27 09:15:37 -0500

    fix: forgot to subscribe to event

--- src/PawsyModules/PinBot/PinBot.cs
+++ src/PawsyModules/PinBot/PinBot.cs
@@ -29,7 +29,7 @@ public class PinBot : GuildModule
     {
         if (Owner.TryGetTarget(out var owner))
         {
-            //Add owner.Event callbacks here
+            owner.OnGuildThreadCreated += ThreadCreated;
         }
     }
 
