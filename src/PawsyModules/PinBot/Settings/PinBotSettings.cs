84919ed7236791433ce4a4dfa3e2355cbc4f46f8
MasterAli2 <seperoa2li@gmail.com>
2024-12-27 09:11:03 -0500

    fix: added JsonInclude to AutoPinChannels

--- src/PawsyModules/PinBot/Settings/PinBotSettings.cs
+++ src/PawsyModules/PinBot/Settings/PinBotSettings.cs
@@ -9,5 +9,6 @@ public class PinBotSettings() : ISettings
     [JsonInclude]
     internal ConcurrentDictionary<ulong, bool> RolesWithPerms = [];
 
+    [JsonInclude]
     internal ConcurrentDictionary<ulong, bool> AutoPinChannels = [];
 }
